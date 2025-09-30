c1f6fb0a42d8089c6bf3dba97e614cb1211461e9
celine <josephinecelinetj@gmail.com>
2025-10-01 01:27:44 +0800

    Fix Checkstyle

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -27,8 +27,8 @@ public class PersonBuilder {
     private Phone phone;
     private Email email;
     private Address address;
-    public Remark remark;
     private Set<Tag> tags;
+    private Remark remark;
 
     /**
      * Creates a {@code PersonBuilder} with the default details.
@@ -105,5 +105,4 @@ public class PersonBuilder {
     public Person build() {
         return new Person(name, phone, email, address, remark, tags);
     }
-
 }
